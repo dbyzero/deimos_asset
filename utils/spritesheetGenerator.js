7f849cdbde4f4185b87b2f69d467ea4c3d3f29a4
dbyzero <cyril@dbyzero.com>
2016-04-07 09:15:19 +0200

    feat: add avatar generation

--- utils/spritesheetGenerator.js
+++ utils/spritesheetGenerator.js
@@ -1,6 +1,7 @@
 var Canvas  = require('canvas');
 var fs      = require('fs');
 var Promise = require('promise');
+var Color   = require('color');
 
 module.exports = {
     /**
@@ -36,7 +37,6 @@ module.exports = {
                         if(err) reject(err);;
                         ctx.drawImage(img,x,y,width,height,x,y,width,height);
                         changeSpritesheetColor(ctx,x,y,width,height,layer.color);
-                        console.log(row.animationName + " done!");
                     };
                     img.src = layer.spritesheet;
                 });
@@ -78,21 +78,41 @@ function changeSpritesheetColor(ctx,x,y,width,height,rgba) {
     if(isNaN(b)) b = 0;
     if(isNaN(a)) a = 255;
 
-    // invert colors
     var r2,g2,b2,a2;
+    var granularity = 1/128;
     for (var i=0;i<imgData.data.length;i+=4)
     {
         r2=imgData.data[i];
         g2=imgData.data[i+1];
         b2=imgData.data[i+2];
         a2=imgData.data[i+3];
-        if(r2 === 0) continue;
+        if(r2 === 0 && g2 === 0 && b2 === 0) {
+            continue;
+        }
         if(r2 === g2 &&  g2 === b2) {
-            imgData.data[i] = (parseInt(r)+r2)/2;
-            imgData.data[i+1] = (parseInt(g)+g2)/2;
-            imgData.data[i+2] = (parseInt(b)+b2)/2;
+            //convert color to a color lib object
+            var pixelColor = Color().rgb(r,g,b);
+            //we scale luminosity compared to 127 ("no change value"), higher is lighten, lower is darken
+            if(r2 > 127) {
+                pixelColor.lighten((r2 - 127) * granularity);
+            } else {
+                pixelColor.darken((127 - r2) * granularity);
+            }
+            //fix to make colors different to not redraw them anymore
+            if(pixelColor.red() === pixelColor.green() || pixelColor.red() === pixelColor.blue()) {
+                imgData.data[i] = (pixelColor.red() === 255) ? 254 : pixelColor.red() + 1;
+            } else {
+                imgData.data[i] = pixelColor.red()
+            }
+            imgData.data[i+1] = pixelColor.green()
+            imgData.data[i+2] = pixelColor.blue()
             imgData.data[i+3] = a;
         }
     }
     ctx.putImageData(imgData,x,y);
+}
+
+function pad (str, max) {
+  str = str.toString();
+  return str.length < max ? pad("0" + str, max) : str;
 }
